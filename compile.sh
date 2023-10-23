e2cc4dbc677663a8e8f63d699942cf30d8fa3169
Edoardo Torrini <edoardo.torrini@gmail.com>
2023-10-23 20:32:45 +0200

    add bash script for compile code usign CMake

---
+++ compile.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [ -d ./build ]; then
+    rm -r ./build
+fi
+
+mkdir build && cd build
+cmake ..
+make
+
+if [ -f main ]; then
+    mv main ../main
+else
+    echo "--------------------[  ERROR on COMPILING  ]--------------------"
+fi
